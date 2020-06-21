8537166eba41e2ff818ef374abfa134dfd91ef8d
Tomas Baca <klaxalk@gmail.com>
2020-06-21 21:58:25 +0200

    updated travis for noetic

--- .ci/ci_test_build.sh
+++ .ci/ci_test_build.sh
@@ -2,8 +2,8 @@
 # author: Robert Penicka
 set -e
 
-echo "Starting test build" 
+echo "Starting test build"
 cd ~/catkin_ws
-source /opt/ros/melodic/setup.bash
+source /opt/ros/$ROS_DISTRO/setup.bash
 catkin build
 echo "Ended test build"
