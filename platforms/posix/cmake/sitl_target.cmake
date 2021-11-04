6509808c6b7d604dcc412f0bfefae151bfd5dbcb
Zejda, Jan <zejdajan@fel.cvut.cz>
2021-11-04 11:44:28 +0100

    Added fully_actuated template without modification, used typhoonh480 with hexa mixer

--- platforms/posix/cmake/sitl_target.cmake
+++ platforms/posix/cmake/sitl_target.cmake
@@ -81,7 +81,8 @@ set(models none shell
 	plane plane_cam plane_catapult plane_lidar
 	standard_vtol tailsitter tiltrotor
 	rover r1_rover boat cloudship
-	uuv_hippocampus)
+	uuv_hippocampus
+	fully_actuated)
 set(worlds none empty baylands ksql_airport mcmillan_airfield sonoma_raceway warehouse windy)
 set(all_posix_vmd_make_targets)
 foreach(viewer ${viewers})
